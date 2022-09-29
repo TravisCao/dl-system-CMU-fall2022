f8127b206923ec22d8180bdbdc030d20018ab9c3
Travis Cao <travisyjcao@gmail.com>
2022-09-30 00:07:47 +0800

    hw0 done

--- assignments/hw0/src/simple_ml.py
+++ assignments/hw0/src/simple_ml.py
@@ -1,6 +1,7 @@
 import struct
 import numpy as np
 import gzip
+
 try:
     from simple_ml_ext import *
 except:
@@ -8,7 +9,7 @@ except:
 
 
 def add(x, y):
-    """ A trivial 'add' function you should implement to get used to the
+    """A trivial 'add' function you should implement to get used to the
     autograder and submission system.  The solution to this problem is in the
     the homework notebook.
 
@@ -20,12 +21,12 @@ def add(x, y):
         Sum of x + y
     """
     ### BEGIN YOUR CODE
-    pass
+    return x + y
     ### END YOUR CODE
 
 
-def parse_mnist(image_filename, label_filename):
-    """ Read an images and labels file in MNIST format.  See this page:
+def parse_mnist(image_filesname, label_filename):
+    """Read an images and labels file in MNIST format.  See this page:
     http://yann.lecun.com/exdb/mnist/ for a description of the file format.
 
     Args:
@@ -34,26 +35,32 @@ def parse_mnist(image_filename, label_filename):
 
     Returns:
         Tuple (X,y):
-            X (numpy.ndarray[np.float32]): 2D numpy array containing the loaded 
-                data.  The dimensionality of the data should be 
-                (num_examples x input_dim) where 'input_dim' is the full 
-                dimension of the data, e.g., since MNIST images are 28x28, it 
-                will be 784.  Values should be of type np.float32, and the data 
-                should be normalized to have a minimum value of 0.0 and a 
-                maximum value of 1.0. The normalization should be applied uniformly
-                across the whole dataset, _not_ individual images.
+            X (numpy.ndarray[np.float32]): 2D numpy array containing the loaded
+                data.  The dimensionality of the data should be
+                (num_examples x input_dim) where 'input_dim' is the full
+                dimension of the data, e.g., since MNIST images are 28x28, it
+                will be 784.  Values should be of type np.float32, and the data
+                should be normalized to have a minimum value of 0.0 and a
+                maximum value of 1.0.
 
-            y (numpy.ndarray[dtype=np.uint8]): 1D numpy array containing the
+            y (numpy.ndarray[dypte=np.uint8]): 1D numpy array containing the
                 labels of the examples.  Values should be of type np.uint8 and
                 for MNIST will contain the values 0-9.
     """
     ### BEGIN YOUR CODE
-    pass
+    with gzip.open(image_filesname) as f:
+        X = (
+            np.frombuffer(f.read(), "B", offset=16).reshape(-1, 784).astype(np.float32)
+            / 255
+        )
+    with gzip.open(label_filename) as f:
+        y = np.frombuffer(f.read(), "B", offset=8).astype(np.uint8)
+    return X, y
     ### END YOUR CODE
 
 
 def softmax_loss(Z, y):
-    """ Return softmax loss.  Note that for the purposes of this assignment,
+    """Return softmax loss.  Note that for the purposes of this assignment,
     you don't need to worry about "nicely" scaling the numerical properties
     of the log-sum-exp computation, but can just compute this directly.
 
@@ -68,12 +75,12 @@ def softmax_loss(Z, y):
         Average softmax loss over the sample.
     """
     ### BEGIN YOUR CODE
-    pass
+    return np.mean(np.log(np.exp(Z).sum(axis=1)) - Z[range(len(Z)), y])
     ### END YOUR CODE
 
 
-def softmax_regression_epoch(X, y, theta, lr = 0.1, batch=100):
-    """ Run a single epoch of SGD for softmax regression on the data, using
+def softmax_regression_epoch(X, y, theta, lr=0.1, batch=100):
+    """Run a single epoch of SGD for softmax regression on the data, using
     the step size lr and specified batch size.  This function should modify the
     theta matrix in place, and you should iterate through batches in X _without_
     randomizing the order.
@@ -83,7 +90,7 @@ def softmax_regression_epoch(X, y, theta, lr = 0.1, batch=100):
             (num_examples x input_dim).
         y (np.ndarray[np.uint8]): 1D class label array of size (num_examples,)
         theta (np.ndarrray[np.float32]): 2D array of softmax regression
-            parameters, of shape (input_dim, num_classes)
+            parameter, of shape (input_dim, num_classes)
         lr (float): step size (learning rate) for SGD
         batch (int): size of SGD minibatch
 
@@ -91,14 +98,25 @@ def softmax_regression_epoch(X, y, theta, lr = 0.1, batch=100):
         None
     """
     ### BEGIN YOUR CODE
-    pass
+    num_samples = X.shape[0]
+    num_classes = theta.shape[1]
+    for i in range(0, num_samples, batch):
+        if i + batch > num_samples:
+            x_batch = X[i:num_samples]
+            y_batch = y[i:num_samples]
+        else:
+            x_batch = X[i : i + batch]
+            y_batch = y[i : i + batch]
+        Z = np.exp(x_batch @ theta) / np.exp(x_batch @ theta).sum(axis=1)[:, np.newaxis]
+        I_y = np.eye(num_classes)[y_batch]
+        theta -= lr / batch * x_batch.T @ (Z - I_y)
     ### END YOUR CODE
 
 
-def nn_epoch(X, y, W1, W2, lr = 0.1, batch=100):
-    """ Run a single epoch of SGD for a two-layer neural network defined by the
+def nn_epoch(X, y, W1, W2, lr=0.1, batch=100):
+    """Run a single epoch of SGD for a two-layer neural network defined by the
     weights W1 and W2 (with no bias terms):
-        logits = ReLU(X * W1) * W2
+        logits = ReLU(X * W1) * W1
     The function should use the step size lr, and the specified batch size (and
     again, without randomizing the order of X).  It should modify the
     W1 and W2 matrices in place.
@@ -107,9 +125,9 @@ def nn_epoch(X, y, W1, W2, lr = 0.1, batch=100):
         X (np.ndarray[np.float32]): 2D input array of size
             (num_examples x input_dim).
         y (np.ndarray[np.uint8]): 1D class label array of size (num_examples,)
-        W1 (np.ndarray[np.float32]): 2D array of first layer weights, of shape
+        W1 (np.ndarrray[np.float32]): 2D array of first layer weights, of shape
             (input_dim, hidden_dim)
-        W2 (np.ndarray[np.float32]): 2D array of second layer weights, of shape
+        W2 (np.ndarrray[np.float32]): 2D array of second layer weights, of shape
             (hidden_dim, num_classes)
         lr (float): step size (learning rate) for SGD
         batch (int): size of SGD minibatch
@@ -118,22 +136,37 @@ def nn_epoch(X, y, W1, W2, lr = 0.1, batch=100):
         None
     """
     ### BEGIN YOUR CODE
-    pass
+    num_samples = X.shape[0]
+    num_classes = W2.shape[1]
+    for i in range(0, num_samples, batch):
+        if i + batch > num_samples:
+            x_batch = X[i:num_samples]
+            y_batch = y[i:num_samples]
+        else:
+            x_batch = X[i : i + batch]
+            y_batch = y[i : i + batch]
+        I_y = np.eye(num_classes)[y_batch]
+        relu = lambda x: np.maximum(x, 0)
+        normalize = lambda x: x / x.sum(axis=1)[:, np.newaxis]
+        Z1 = relu(x_batch @ W1)
+        G2 = normalize(np.exp(Z1 @ W2)) - I_y
+        G1 = (Z1 > 0).astype(int) * (G2 @ W2.T)
+        W1 -= lr / batch * (x_batch.T @ G1)
+        W2 -= lr / batch * (Z1.T @ G2)
     ### END YOUR CODE
-
 
 
 ### CODE BELOW IS FOR ILLUSTRATION, YOU DO NOT NEED TO EDIT
 
-def loss_err(h,y):
-    """ Helper funciton to compute both loss and error"""
-    return softmax_loss(h,y), np.mean(h.argmax(axis=1) != y)
+
+def loss_err(h, y):
+    """Helper funciton to compute both loss and error"""
+    return softmax_loss(h, y), np.mean(h.argmax(axis=1) != y)
 
 
-def train_softmax(X_tr, y_tr, X_te, y_te, epochs=10, lr=0.5, batch=100,
-                  cpp=False):
-    """ Example function to fully train a softmax regression classifier """
-    theta = np.zeros((X_tr.shape[1], y_tr.max()+1), dtype=np.float32)
+def train_softmax(X_tr, y_tr, X_te, y_te, epochs=10, lr=0.5, batch=100, cpp=False):
+    """Example function to fully train a softmax regression classifier"""
+    theta = np.zeros((X_tr.shape[1], y_tr.max() + 1), dtype=np.float32)
     print("| Epoch | Train Loss | Train Err | Test Loss | Test Err |")
     for epoch in range(epochs):
         if not cpp:
@@ -142,13 +175,15 @@ def train_softmax(X_tr, y_tr, X_te, y_te, epochs=10, lr=0.5, batch=100,
             softmax_regression_epoch_cpp(X_tr, y_tr, theta, lr=lr, batch=batch)
         train_loss, train_err = loss_err(X_tr @ theta, y_tr)
         test_loss, test_err = loss_err(X_te @ theta, y_te)
-        print("|  {:>4} |    {:.5f} |   {:.5f} |   {:.5f} |  {:.5f} |"\
-              .format(epoch, train_loss, train_err, test_loss, test_err))
+        print(
+            "|  {:>4} |    {:.5f} |   {:.5f} |   {:.5f} |  {:.5f} |".format(
+                epoch, train_loss, train_err, test_loss, test_err
+            )
+        )
 
 
-def train_nn(X_tr, y_tr, X_te, y_te, hidden_dim = 500,
-             epochs=10, lr=0.5, batch=100):
-    """ Example function to train two layer neural network """
+def train_nn(X_tr, y_tr, X_te, y_te, hidden_dim=500, epochs=10, lr=0.5, batch=100):
+    """Example function to train two layer neural network"""
     n, k = X_tr.shape[1], y_tr.max() + 1
     np.random.seed(0)
     W1 = np.random.randn(n, hidden_dim).astype(np.float32) / np.sqrt(hidden_dim)
@@ -157,21 +192,25 @@ def train_nn(X_tr, y_tr, X_te, y_te, hidden_dim = 500,
     print("| Epoch | Train Loss | Train Err | Test Loss | Test Err |")
     for epoch in range(epochs):
         nn_epoch(X_tr, y_tr, W1, W2, lr=lr, batch=batch)
-        train_loss, train_err = loss_err(np.maximum(X_tr@W1,0)@W2, y_tr)
-        test_loss, test_err = loss_err(np.maximum(X_te@W1,0)@W2, y_te)
-        print("|  {:>4} |    {:.5f} |   {:.5f} |   {:.5f} |  {:.5f} |"\
-              .format(epoch, train_loss, train_err, test_loss, test_err))
-
+        train_loss, train_err = loss_err(np.maximum(X_tr @ W1, 0) @ W2, y_tr)
+        test_loss, test_err = loss_err(np.maximum(X_te @ W1, 0) @ W2, y_te)
+        print(
+            "|  {:>4} |    {:.5f} |   {:.5f} |   {:.5f} |  {:.5f} |".format(
+                epoch, train_loss, train_err, test_loss, test_err
+            )
+        )
 
 
 if __name__ == "__main__":
-    X_tr, y_tr = parse_mnist("data/train-images-idx3-ubyte.gz",
-                             "data/train-labels-idx1-ubyte.gz")
-    X_te, y_te = parse_mnist("data/t10k-images-idx3-ubyte.gz",
-                             "data/t10k-labels-idx1-ubyte.gz")
+    X_tr, y_tr = parse_mnist(
+        "data/train-images-idx3-ubyte.gz", "data/train-labels-idx1-ubyte.gz"
+    )
+    X_te, y_te = parse_mnist(
+        "data/t10k-images-idx3-ubyte.gz", "data/t10k-labels-idx1-ubyte.gz"
+    )
 
     print("Training softmax regression")
-    train_softmax(X_tr, y_tr, X_te, y_te, epochs=10, lr = 0.1)
+    train_softmax(X_tr, y_tr, X_te, y_te, epochs=10, lr=0.1)
 
     print("\nTraining two layer neural network w/ 100 hidden units")
-    train_nn(X_tr, y_tr, X_te, y_te, hidden_dim=100, epochs=20, lr = 0.2)
+    train_nn(X_tr, y_tr, X_te, y_te, hidden_dim=100, epochs=20, lr=0.2)
--- assignments/hw0/src/simple_ml_ext.cpp
+++ assignments/hw0/src/simple_ml_ext.cpp
@@ -5,10 +5,9 @@
 
 namespace py = pybind11;
 
-
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
-								  float *theta, size_t m, size_t n, size_t k,
-								  float lr, size_t batch)
+                                  float *theta, size_t m, size_t n, size_t k,
+                                  float lr, size_t batch)
 {
     /**
      * A C++ version of the softmax regression epoch code.  This should run a
@@ -20,9 +19,9 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      *     X (const float *): pointer to X data, of size m*n, stored in row
      *          major (C) format
      *     y (const unsigned char *): pointer to y data, of size m
-     *     theta (float *): pointer to theta data, of size n*k, stored in row
+     *     theta (foat *): pointer to theta data, of size n*k, stored in row
      *          major (C) format
-     *     m (size_t): number of examples
+     *     m (size_t): number of exmaples
      *     n (size_t): input dimension
      *     k (size_t): number of classes
      *     lr (float): learning rate / SGD step size
@@ -33,34 +32,130 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      */
 
     /// BEGIN YOUR CODE
+    for (size_t sample_i = 0; sample_i < m; sample_i += batch)
+    {
+        size_t size = 0;
+        size_t n_row, n_col = 0;
+        if (sample_i + batch > m)
+        {
+            size = m - sample_i;
+        }
+        else
+        {
+            size = batch;
+        }
+        float x_batch[size * n];
+        unsigned char y_batch[size];
 
+        n_row = size;
+        n_col = n;
+        for (size_t i = 0; i < n_row; i++)
+        {
+            for (size_t j = 0; j < n_col; j++)
+            {
+                x_batch[i * n_col + j] = X[(sample_i + i) * n_col + j];
+            }
+            y_batch[i] = y[sample_i + i];
+        }
+
+        float x_theta_mul[size * k] = {0};
+        float Z[size * k] = {0};
+        float I_y[size * k] = {0};
+
+        // x_theta_mul (size * n) * (n * k) -> (size * k)
+        n_row = size;
+        n_col = k;
+        for (size_t i = 0; i < n_row; i++)
+        {
+            for (size_t j = 0; j < n_col; j++)
+            {
+                for (size_t jj = 0; jj < n; jj++)
+                {
+                    x_theta_mul[i * n_col + j] += x_batch[i * n + jj] * theta[jj * k + j];
+                }
+            }
+        }
+
+        // Z is of size (size * k)
+        n_row = size;
+        n_col = k;
+        for (size_t i = 0; i < n_row; i++)
+        {
+            float row_sum = 1e-8;
+            for (size_t j = 0; j < n_col; j++)
+            {
+                Z[i * n_col + j] = std::exp(x_theta_mul[i * n_col + j]);
+                row_sum += Z[i * n_col + j];
+            }
+            for (size_t j = 0; j < n_col; j++)
+            {
+                Z[i * n_col + j] /= row_sum;
+            }
+        }
+
+        for (size_t i = 0; i < size; i++)
+        {
+            int label = int(y_batch[i]);
+            I_y[i * k + label] = 1;
+        }
+
+        float x_batch_T[n * size] = {0};
+        float x_batch_T_mul[n * k] = {0};
+
+        n_row = size;
+        n_col = n;
+        for (size_t i = 0; i < n_row; i++)
+        {
+            for (size_t j = 0; j < n_col; j++)
+            {
+                x_batch_T[j * n_row + i] = x_batch[i * n_col + j];
+            }
+        }
+
+        // x_batch_T_mul: x_batch.T * (Z - I_y)
+        //               (n * size) * (size * k) -> (n * k)
+        n_row = n;
+        n_col = k;
+        for (size_t i = 0; i < n_row; i++)
+        {
+            for (size_t j = 0; j < n_col; j++)
+            {
+                for (size_t jj = 0; jj < size; jj++)
+                {
+                    x_batch_T_mul[i * n_col + j] += x_batch_T[i * size + jj] * (Z[jj * k + j] - I_y[jj * k + j]);
+                }
+                theta[i * n_col + j] -= lr / int(size) * x_batch_T_mul[i * n_col + j];
+            }
+        }
+    }
     /// END YOUR CODE
 }
-
 
 /**
  * This is the pybind11 code that wraps the function above.  It's only role is
  * wrap the function above in a Python module, and you do not need to make any
  * edits to the code
  */
-PYBIND11_MODULE(simple_ml_ext, m) {
-    m.def("softmax_regression_epoch_cpp",
-    	[](py::array_t<float, py::array::c_style> X,
+PYBIND11_MODULE(simple_ml_ext, m)
+{
+    m.def(
+        "softmax_regression_epoch_cpp",
+        [](py::array_t<float, py::array::c_style> X,
            py::array_t<unsigned char, py::array::c_style> y,
            py::array_t<float, py::array::c_style> theta,
            float lr,
-           int batch) {
-        softmax_regression_epoch_cpp(
-        	static_cast<const float*>(X.request().ptr),
-            static_cast<const unsigned char*>(y.request().ptr),
-            static_cast<float*>(theta.request().ptr),
-            X.request().shape[0],
-            X.request().shape[1],
-            theta.request().shape[1],
-            lr,
-            batch
-           );
-    },
-    py::arg("X"), py::arg("y"), py::arg("theta"),
-    py::arg("lr"), py::arg("batch"));
+           int batch)
+        {
+            softmax_regression_epoch_cpp(
+                static_cast<const float *>(X.request().ptr),
+                static_cast<const unsigned char *>(y.request().ptr),
+                static_cast<float *>(theta.request().ptr),
+                X.request().shape[0],
+                X.request().shape[1],
+                theta.request().shape[1],
+                lr,
+                batch);
+        },
+        py::arg("X"), py::arg("y"), py::arg("theta"),
+        py::arg("lr"), py::arg("batch"));
 }
